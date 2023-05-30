8140472343d523fd9a64375b897a93211d3a7680
MAfzal <mohamadafzal06@gmail.com>
2023-05-30 18:42:46 +0330

    develop: add simple handers and add pgadmin for postgres db

---
+++ pkg/random_generator/randomString.go
@@ -0,0 +1,16 @@
+package randomgenerator
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandStringRunes(n int) string {
+	rand.Seed(time.Now().UnixNano())
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
