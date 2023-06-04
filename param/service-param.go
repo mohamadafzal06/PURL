eed7a062ae2fe0f408340c2d6ea69709b973bb04
MAfzal <mohamadafzal06@gmail.com>
2023-06-04 10:31:12 +0330

    develop: add simple redis repository for caching

--- param/service-param.go
+++ param/service-param.go
@@ -1,10 +1,18 @@
 package param
 
+import "time"
+
 type ShortRequest struct {
-	LongURL string `json:"long_url"`
+	URL    string        `json:"url"`
+	Expiry time.Duration `json:"expiry"`
 }
+
 type ShortResponse struct {
-	Key string `json:"key"`
+	URL             string        `json:"url"`
+	Key             string        `json:"key"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
 
 type LongRequest struct {
